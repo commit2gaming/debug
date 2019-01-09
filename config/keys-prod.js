52db6684b631fcb2becd1fce912c900b792e14d0
Camilo <camilo.c@protonmail.com>
2019-01-10 00:48:58 +0100

    Minor changes

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -2,6 +2,6 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	cookieKey: 'This is a completely random string'
+	cookieKey: process.env.COOKIE_KEY
 }
 
