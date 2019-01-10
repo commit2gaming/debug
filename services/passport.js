ff6dda24e0b8996d7e827c7cdfb9e01fd009b429
Camilo <camilo.c@protonmail.com>
2019-01-10 14:20:31 +0100

    Integration of BE NodeJS auth and FE React Redux boilerplate

--- services/passport.js
+++ services/passport.js
@@ -25,21 +25,11 @@ passport.use(
 		callbackURL: "/auth/google/callback",
 		proxy: true
 	},
-		((accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id })
-				.then(user => {
-					if (user) {
-						// Already have a record
-						done(null, user);
-						console.log('User already exists');
-					} else {
-						// Create new user
-						new User({ googleId: profile.id })
-							.save()
-							.then(user => done(null, user));
-					}
-				})
-		})
+		async (accessToken, refreshToken, profile, done) => {
+			let user = await User.findOne({ googleId: profile.id });
+			if (!user) user = await new User({ googleId: profile.id }).save();
+			done(null, user);
+		}
 	)
 );
 
